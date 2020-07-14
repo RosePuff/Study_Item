4175ebe76e6a80b2f669285667460af25db38143
RosePuff <LiuQinHua2020@outlook.com>
2020-07-14 09:44:03 +0800

    SpringIOC

---
+++ Spring-study/spring_01_ioc/src/main/java/com/chin/dao/UserDaoDeleteImpl.java
@@ -0,0 +1,15 @@
+package com.chin.dao;
+
+/**
+ * @ClassName: UserDaoDeleteImpl
+ * @Description: TODO 类描述
+ * @Author: LQH
+ * @Date: 2020/07/13
+ * @Version: 1.0
+ **/
+public class UserDaoDeleteImpl implements UserDao {
+    @Override
+    public void getUser() {
+        System.out.println("用户删除数据");
+    }
+}
--- Spring-study/spring_01_ioc/src/test/java/NewTest.java
+++ Spring-study/spring_01_ioc/src/test/java/NewTest.java
@@ -1,3 +1,5 @@
+import com.chin.dao.UserDaoDeleteImpl;
+import com.chin.service.UserService;
 import com.chin.service.UserServiceImpl;
 
 /**
@@ -12,7 +14,12 @@ public class NewTest {
     public static void main(String[] args) {
 
         //实际是使用的是业务层调用，Dao层不会使用
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
+
+        // ((UserServiceImpl) userService).setUserDao(new UserDaoImpl());
+        ((UserServiceImpl) userService).setUserDao(new UserDaoDeleteImpl());
+
         userService.getUser();
+
     }
 }
