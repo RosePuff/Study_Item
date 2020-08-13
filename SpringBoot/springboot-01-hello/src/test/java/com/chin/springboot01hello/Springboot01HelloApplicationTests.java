8b0a831b8425ad8762a6064d774b723d421fd29d
RosePuff <LiuQinHua2020@outlook.com>
2020-08-13 18:04:36 +0800

    springboot初始化

---
+++ SpringBoot/springboot-01-hello/src/test/java/com/chin/springboot01hello/Springboot01HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chin.springboot01hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01HelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
